229afd0b035fe22496c89374ed26e0348225eb46
AhsanAli <ahhsan87@gmail.com>
2019-09-17 12:38:12 +0500

    Fixed error on init event of createEmployee

--- src/app/create-employee/create-employee.component.ts
+++ src/app/create-employee/create-employee.component.ts
@@ -36,6 +36,7 @@ export class CreateEmployeeComponent implements OnInit {
   newEmployee: boolean = false; //Check if the request is to save a new employee or Update employee
   employee: Employee
   constructor(private dataService: DataService, private route: ActivatedRoute, private location: Location) {
+    
   }
   addEmployee() {
     this.dataService.addEmployee(this.empObj);
@@ -52,6 +53,7 @@ export class CreateEmployeeComponent implements OnInit {
   }
 
   ngOnInit() {
+   // debugger;
     this.canvas = document.getElementById('myCanvas');
     this.canvas.width = 960
     this.canvas.height = 630
@@ -108,6 +110,7 @@ export class CreateEmployeeComponent implements OnInit {
   }
   previousMarker: any = [];
   drawMarker(event) {
+    
     console.log('Before setting the points on click X: ', this.empObj.positionX)
     console.log('Before setting the points on click Y: ', this.empObj.positionY)
 
@@ -123,13 +126,15 @@ export class CreateEmployeeComponent implements OnInit {
     map.onload = () => {
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
       this.ctx.drawImage(map, 0, 0, 950, 620);
-      //if (event) {
+      if (event) {
         this.empObj.positionX = this.x;
         this.empObj.positionY = this.y;
+        console.log('X: ',this.empObj.positionX);
+        console.log('Y: ',this.empObj.positionY);
         this.ctx.drawImage(marker, this.x, this.y, 28, 30);
         // this.empObj.positionX = this.x;
         // this.empObj.positionY = this.y;
-      //}
+      }
     }
   }
 }
